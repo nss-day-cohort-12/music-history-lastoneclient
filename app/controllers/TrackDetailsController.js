f37128fcc47987fa31c2b155e42af482a10f1ca7
eorstrom <eorstrom2010@gmail.com>
2016-06-13 09:30:21 -0500

    song details appear on the page for individual tracks when selected.

--- app/controllers/TrackDetailsController.js
+++ app/controllers/TrackDetailsController.js
@@ -4,14 +4,16 @@ MusicHistory.controller('TrackDetailsController', [
     '$http', 
     '$scope',
     'AuthFactory',
+    '$routeParams',
 
-    function ($http, $scope, AuthFactory) {
+    function ($http, $scope, AuthFactory, $routeParams) {
 
         $scope.track = null;
         $scope.user = AuthFactory.getUser();
+        console.log($routeParams)
 
         $http
-            .get('http://localhost:5000/api/Tracks/2')
+            .get(`http://localhost:5000/api/Tracks/${$routeParams.id}`)
             .success(inv => $scope.track = inv[0]);
         }
 
